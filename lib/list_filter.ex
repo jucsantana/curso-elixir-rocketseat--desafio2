31c5e14ab4e7e3a9599ee78fa9583f14476010a1
jucsantana <jucsantana@gmail.com>
2021-11-11 10:22:14 -0300

    renomeada a clausula para o padrao snake_case

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,14 +1,15 @@
 defmodule ListFilter do
-  def filter(list), do: countNumberOdd(list)
 
-  defp countNumberOdd([]), do: 0
+    def filter(list), do: count_number_odd(list)
 
-  defp countNumberOdd(list) do
-    list
-    |> Enum.map(fn x -> Integer.parse(x) end)
-    |> Enum.filter(fn x -> is_tuple(x) end)
-    |> Enum.map(fn x -> elem(x, 0) end)
-    |> Enum.filter(fn x -> rem(x, 2) != 0 end)
-    |> length()
-  end
+    defp count_number_odd([]), do: 0
+
+    defp count_number_odd(list) do
+      list
+        |>Enum.map(fn x -> Integer.parse(x) end) 
+        |>Enum.filter(fn x -> is_tuple(x) end)
+        |>Enum.map(fn x -> elem(x,0) end)
+        |>Enum.filter(fn x -> rem(x,2)!=0 end)
+        |>length()
+    end
 end
